7e72603f1b9997249652e3305ae9c02f361fe16a
uyni <nguyenuyennhi.work@gmail.com>
2024-09-09 18:55:56 +0700

    Update HomeContrller

--- Bakini_ECommerce/Controllers/HomeController.cs
+++ Bakini_ECommerce/Controllers/HomeController.cs
@@ -1,21 +1,27 @@
-using Bakini_ECommerce.Models;
+using WebBanHang0.Models;
+using WebBanHang0.Repository;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Bakini_ECommerce.Models;
 
-namespace Bakini_ECommerce.Controllers
+namespace WebBanHang0.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly DatabaseContext _databaseContext;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, DatabaseContext context)
         {
             _logger = logger;
+            _databaseContext = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var products = _databaseContext.Products.Include("Category").Include("Brand").ToList();
+            return View(products);
         }
 
         public IActionResult Privacy()
@@ -24,9 +30,17 @@ namespace Bakini_ECommerce.Controllers
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Error(int statuscode)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (statuscode == 404)
+            {
+                return View("NotFound");
+
+            }
+            else
+            {
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
         }
     }
 }
