86014d2761221bc270f4af60083bb01bcb929f8b
trakie <trantuankiet.authentic@gmail.com>
2024-09-10 23:20:53 +0700

    Add ViewOrder Admin Update OrderController Admin

---
+++ Bakini_ECommerce/Areas/Admin/ViewModels/OrderViewModel.cs
@@ -0,0 +1,10 @@
+﻿using WebBanHang0.Models;
+
+namespace WebBanHang0.Areas.Admin.Models.ViewModels
+{
+    public class OrderViewModel
+    {
+        public IEnumerable<OrderDetails> OrderDetails { get; set; }
+        public IEnumerable<OrderModel> OrderModel { get; set; }
+    }
+}
